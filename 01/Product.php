9cd117ece23f064a4a3f0295350091368bfa4497
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-01 18:47:34 +0200

    mise en place des getters et setters et de la méthode updateTimeStamp pour mettre à jour l'attributs updatedAt quand un modifie un attribut de produits

--- 01/Product.php
+++ 01/Product.php
@@ -21,6 +21,70 @@ class Product {
         $this->createdAt = new DateTime(); 
         $this->updatedAt = new DateTime(); 
     }
+    // Getters (accesseurs)
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getPhotos(): string {
+        return $this->photos;
+    }
+
+    public function getPrice(): int {
+        return $this->price;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function getQuantity(): int {
+        return $this->quantity;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    // Setters (mutateurs)
+    public function setName(string $name): void {
+        $this->name = $name;
+        $this->updateTimestamp();
+    }
+
+    public function setPhotos(string $photos): void {
+        $this->photos = $photos;
+        $this->updateTimestamp();
+    }
+
+    public function setPrice(int $price): void {
+        $this->price = $price;
+        $this->updateTimestamp();
+    }
+
+    public function setDescription(string $description): void {
+        $this->description = $description;
+        $this->updateTimestamp();
+    }
+
+    public function setQuantity(int $quantity): void {
+        $this->quantity = $quantity;
+        $this->updateTimestamp();
+    }
+
+    // Méthode pour mettre à jour le timestamp de updatedAt
+    private function updateTimestamp(): void {
+        $this->updatedAt = new DateTime(); // Mise à jour de updatedAt à la date et l'heure actuelles
+    }
+
 }
 
 ?>
