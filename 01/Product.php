97e2e138bbb2ce999815edda676fedca8cc02827
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-01 18:38:44 +0200

    création des attributs de la classe Product

---
+++ 01/Product.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Product {
+    private int $id;
+    private string $name;
+    private string $photos;
+    private int $price;
+    private string $description;
+    private int $quantity;
+    private DateTime $createdAt;
+    private DateTime $updateAt;
+
+
+}
+
+?>
