67e60f531e977f12111d6a286bd963bf12097f2b
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-01 22:17:39 +0200

    création de l'objet category avec ces attributs

---
+++ 02/Category.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Category {
+
+    private int $id;
+    private string $name;
+    private string $description;
+    private DateTime $createdAt;
+    private DateTime $uptadedAt;
+
+}
