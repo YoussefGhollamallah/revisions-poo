e9ab05d2fee491e009080e5219f962587bfb8f37
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-02 14:09:20 +0200

    modification de l'index pour permettre le changement de pages

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once __DIR__ . "/02/Category.php";
 $firstCategory = new Category(1, "Chien", "les chiens faut partie des canidées.");
 $firstProduct = new Product(1, "Chien", ["https://picsum.photos/id/237/200/300"],1250, "Bébé Labrador", 3, 1, new DateTime(), new DateTime());
 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +15,12 @@ $firstProduct = new Product(1, "Chien", ["https://picsum.photos/id/237/200/300"]
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>Document</title>
     </head>
     <body>
+        
+        <?php  include_once __DIR__ . "/includes/_header.php"; ?>
         
         <!-- Affichage des informations du produit -->
         <!-- <h1><?php echo $firstProduct->getName(); ?></h1> -->
@@ -24,13 +29,30 @@ $firstProduct = new Product(1, "Chien", ["https://picsum.photos/id/237/200/300"]
         <!-- <p><?php echo $firstProduct->getDescription(); ?></p> -->
         <!-- <p>Prix : <?php echo $firstProduct->getPrice(); ?> €</p> -->
         <!-- <p>Quantité disponible : <?php echo $firstProduct->getQuantity(); ?></p> -->        
-        
+        <main>
+
+            <?php
+            // Définit la page par défaut
+            $page = isset($_GET['page']) ? $_GET['page'] : 'index';
+            
+            // Chemin du fichier correspondant à la page
+            $file = __DIR__ . "/pages/" . $page . ".php";
+            
+            // Vérifie si le fichier existe, sinon, affiche une erreur
+            if (file_exists($file)) {
+                require_once($file);
+            } else {
+                require_once (__DIR__. "/pages/page404.php");
+            }
+            ?>
+        </main>
         <?php 
-            var_dump($firstProduct);
-            echo $firstProduct->getName();
-            $firstProduct->setName("Labrador");
-            echo "<br>";
-            echo $firstProduct->getName();
+            // require_once "pages/addProduct.php";
+            // var_dump($firstProduct);
+            // echo $firstProduct->getName();
+            // $firstProduct->setName("Labrador");
+            // echo "<br>";
+            // echo $firstProduct->getName();
             
         ?>
     </body>
