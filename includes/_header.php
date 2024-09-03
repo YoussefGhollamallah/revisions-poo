a8713d82c117ed779d7d9291b4448c94a355062e
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-03 16:47:24 +0200

    modification des liens de navigation

--- includes/_header.php
+++ includes/_header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
         <a class="navbar-brand" href="#">Menu</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -7,13 +7,13 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../index.php">Home</a>
+                    <a class="nav-link" href="index.php?page=index">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Produits</a>
+                    <a class="nav-link" href="index.php?page=produit">Produits</a>
                 </li>
                 <li>
-                    <a class="nav-link" href="pages/addProduct.php">addProduct</a>
+                    <a class="nav-link" href="index.php?page=addProduct">addProduct</a>
 
                 </li>
             </ul>
